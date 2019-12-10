9fd31e4e7382b7f82e86e97078f633406359a3b0
rahulgorremutchu <rahul.gorremutchu@gmail.com>
2019-12-09 21:00:18 -0500

    Working on HomeActivity

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/PayrollDetails.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/PayrollDetails.java
@@ -1,4 +1,14 @@
 package com.example.group12_f2019_mad3125_fp;
 
-public class PayrollDetails {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PayrollDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payroll_details);
+    }
 }
