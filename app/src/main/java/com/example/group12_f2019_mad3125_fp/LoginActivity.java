aaca2c9f669c4c4f3c6fca256fb5dcea90e643fa
rahulgorremutchu <rahul.gorremutchu@gmail.com>
2019-12-09 19:26:24 -0500

    Declared Variables - LoginActivity.java

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.group12_f2019_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,6 +18,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        
+        Email = (EditText) findViewById(R.id.txt_email);
+        Password = (EditText) findViewById(R.id.txt_password);
+        Login = (Button) findViewById(R.id.btn_login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+
+        });
     }
 }
+
