2ffca263eac5cc75a604d31e577b1fa0349ceb50
rahulgorremutchu <rahul.gorremutchu@gmail.com>
2019-12-09 19:19:46 -0500

    Created Variables - LoginActivity.java

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/LoginActivity.java
@@ -3,12 +3,20 @@ package com.example.group12_f2019_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        
     }
 }
