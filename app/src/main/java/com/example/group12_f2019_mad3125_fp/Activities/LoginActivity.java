34b2938c3e19a5eb3c2362ef98a110a8e86c5580
rahulgorremutchu <rahul.gorremutchu@gmail.com>
2019-12-11 17:24:34 -0500

    Final Commit

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/Activities/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.group12_f2019_mad3125_fp.R;
@@ -17,54 +18,51 @@ import static android.text.TextUtils.isEmpty;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private String username;
-    private String password;
-    private EditText Email;
+    private EditText Name;
     private EditText Password;
     private Button Login;
-    Preference preference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        preference = new Preference(this);
 
-        username = "user@employee.com";
-        password = "s3cr3t";
-
-        Email = findViewById(R.id.txt_email);
-        Password = findViewById(R.id.txt_password);
+        Name = (EditText) findViewById(R.id.txt_email);
+        Password = (EditText) findViewById(R.id.txt_password);
+        Login = (Button) findViewById(R.id.btn_login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
     }
 
-    public void onLoginClick(View view){
-        String sEmail = Email.getText().toString();
-        String sPswd = Password.getText().toString();
-        if (isEmpty(sEmail) || isEmpty(sPswd)){
-            Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-        } else {
-            if (!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches()){
-                Email.setError("Invalid email");
-            } else {
-                if (sEmail.equals(username) && sPswd.equals(password)){
-                    loginSuccess();
-                } else {
-                    Toast.makeText(this, "Invalid email/password", Toast.LENGTH_SHORT).show();
-                    Email.requestFocus();
-                }
-            }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("user@employee.com")) && (userPassword.equals("s3cr3t"))) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         }
 
-    }
 
-    private void loginSuccess() {
-        
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (userName.isEmpty() && userPassword.equals("s3cr3t")) {
+            Name.setError("empty");
+        } else if (userName.equals("user@employee.com") && userPassword.isEmpty()) {
+            Password.setError("empty");
+        } else if (userName.equals("user@employee.com") && userPassword != "s3cr3t") {
+            Password.setError("invalid password");
+        } else if (userName != "user@employee.com" && userPassword.equals("s3cr3t")) {
+            Name.setError("invalid username");
+        } else
+        {
+
+            Name.setError("invalid username");
+            Password.setError("invalid password");
+
+        }
     }
 }
-
-
-
--- app/src/main/java/com/example/group12_f2019_mad3125_fp/Preference.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/Preference.java
@@ -3,39 +3,36 @@ package com.example.group12_f2019_mad3125_fp;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+public class Preference {
+    private SharedPreferences preferences;
 
-    public class Preference {
-        private SharedPreferences preferences;
-
-        public Preference(Context context){
-            preferences = context.getSharedPreferences("myPreference", Context.MODE_PRIVATE);
-        }
-
-        public void put(String key, String value){
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putString(key, value);
-            editor.apply();
-        }
-
-        public void put(String key, boolean value){
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean(key, value);
-            editor.apply();
-        }
-
-
-        public boolean getBoolean(String key){
-            return preferences.getBoolean(key, false);
-        }
-
-        public String getString(String key){
-            return preferences.getString(key, "");
-        }
-
-        public void clearData(String key){
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.remove(key);
-            editor.apply();
-        }
+    public Preference(Context context){
+        preferences = context.getSharedPreferences("myPreference", Context.MODE_PRIVATE);
     }
 
+    public void put(String key, String value){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public void put(String key, boolean value){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public boolean getBoolean(String key){
+        return preferences.getBoolean(key, false);
+    }
+
+    public String getString(String key){
+        return preferences.getString(key, "");
+    }
+
+    public void clearData(String key){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove(key);
+        editor.apply();
+    }
+}
