fe35e9dedde396c8323619dd981898a8fbb45929
Srinath <srinathballa96@gmail.com>
2019-12-11 10:03:47 -0500

    Created method ongetCount() - PayrollAdapter.java

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/Adapters/PayrollAdapter.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/Adapters/PayrollAdapter.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.group12_f2019_mad3125_fp.EmployeeModels.Employee;
 import com.example.group12_f2019_mad3125_fp.Interfaces.OnPayrollClicked;
+import com.example.group12_f2019_mad3125_fp.R;
 
 import java.util.List;
 
@@ -39,4 +42,28 @@ public class PayrollAdapter {
         holder.tvVehicle.setText(employee.getVehicle().getIsA()+" : "+employee.getVehicle().getPlate());
     }
 
+    @Override
+    public int getItemCount() {
+        return employeeList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvName, tvJob, tvVehicle;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvJob   = itemView.findViewById(R.id.tv_job);
+            tvVehicle = itemView.findViewById(R.id.tv_vehicle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onPayrollClicked.onPayrollClicked(getAdapterPosition());
+                }
+            });
+        }
+    }
 }
+
+
