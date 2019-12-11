46654237636b0f9a09d0abe72b7b59f776c8fbf8
Srinath <srinathballa96@gmail.com>
2019-12-11 09:56:08 -0500

    Created Variables

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/Adapters/PayrollAdapter.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/Adapters/PayrollAdapter.java
@@ -1,4 +1,15 @@
 package com.example.group12_f2019_mad3125_fp.Adapters;
 
+import android.content.Context;
+
+import com.example.group12_f2019_mad3125_fp.EmployeeModels.Employee;
+import com.example.group12_f2019_mad3125_fp.Interfaces.OnPayrollClicked;
+
+import java.util.List;
+
 public class PayrollAdapter {
+
+    private Context context;
+    private List<Employee> employeeList;
+    private OnPayrollClicked onPayrollClicked;
 }
