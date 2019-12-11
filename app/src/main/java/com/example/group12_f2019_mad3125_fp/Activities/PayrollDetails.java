fc4d6cfc494ff7824f921d291e489f76d583cc37
Srinath <srinathballa96@gmail.com>
2019-12-10 23:33:02 -0500

    Declared Variables - PayrollDetails.java

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/Activities/PayrollDetails.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/Activities/PayrollDetails.java
@@ -3,16 +3,27 @@ package com.example.group12_f2019_mad3125_fp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.group12_f2019_mad3125_fp.EmployeeModels.Employee;
 import com.example.group12_f2019_mad3125_fp.R;
 
 public class PayrollDetails extends AppCompatActivity {
+
+    Employee employee;
+    TextView tv_job_one, tv_job_two, tv_job_three, tv_job_type, tv_vehicle, tv_earning, tv_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payroll_details);
+
+        tv_job_three = findViewById(R.id.tv_job_three);
+        tv_job_two = findViewById(R.id.tv_job_two);
+        tv_job_one = findViewById(R.id.tv_job_one);
+        tv_job_type = findViewById(R.id.tv_job_type);
+        tv_vehicle = findViewById(R.id.tv_vehicle);
+        tv_earning = findViewById(R.id.tv_earning);
+        tv_name = findViewById(R.id.tv_name);
     }
 }
-//hi
-//hi
