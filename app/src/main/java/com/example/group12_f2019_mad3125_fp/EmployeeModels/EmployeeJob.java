e4f3dbacc3c2c894c59533499465243ca9a74fef
Srinath <srinathballa96@gmail.com>
2019-12-11 01:43:16 -0500

    Added setter - setPartTimeType()

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/EmployeeModels/EmployeeJob.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/EmployeeModels/EmployeeJob.java
@@ -83,4 +83,8 @@ public class EmployeeJob {
     {
         return partTimeType;
     }
+    public void setPartTimeType(String partTimeType)
+    {
+        this.partTimeType = partTimeType;
+    }
 }
