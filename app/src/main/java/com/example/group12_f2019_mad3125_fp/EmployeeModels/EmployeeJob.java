2fa8dbc723230709e06fbaca39fb63269996e885
Srinath <srinathballa96@gmail.com>
2019-12-11 01:27:43 -0500

    Declared Variables

--- app/src/main/java/com/example/group12_f2019_mad3125_fp/EmployeeModels/EmployeeJob.java
+++ app/src/main/java/com/example/group12_f2019_mad3125_fp/EmployeeModels/EmployeeJob.java
@@ -1,4 +1,13 @@
 package com.example.group12_f2019_mad3125_fp.EmployeeModels;
 
 public class EmployeeJob {
+    private String jobType;
+    private String partTimeType;
+    private double salary;
+    private double bonus;
+    private String school;
+    private double rate;
+    private double hours;
+    private double commission;
+    private double fixed;
 }
